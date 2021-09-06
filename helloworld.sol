c0f34168652424b5f52667b69ffa2af29b8a4713
veshen <veshen0727@gmail.com>
2021-09-06 14:09:21 +0800

    pure view

--- helloworld.sol
+++ helloworld.sol
@@ -3,11 +3,21 @@ pragma solidity ^0.4.0;
 contract Helloworld {
     string Myname = "helloworld";
 
+    /**
+        view 不消耗eth
+     */
     function getName() public view returns (string) {
         return Myname;
     }
 
     function changeName(string _newName) public {
         Myname = _newName;
+    }
+
+    /**
+        pure 不会读取数据 固定的输入输出 不消耗
+     */
+    function pureTest(string _name) public pure returns (string) {
+        return _name;
     }
 }
