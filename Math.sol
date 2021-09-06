aeab14216475aeeca0c1f9ef0d5ce19c866cefff
veshen <veshen0727@gmail.com>
2021-09-06 15:06:42 +0800

    位操作

--- Math.sol
+++ Math.sol
@@ -10,7 +10,7 @@ contract math{
      */
 
     int numa = 100; //int256 == int 
-    uint numb = 200; //uint 256
+    uint numb = 200; //uint  256
 
     function add(uint a,uint b) 
         public
@@ -60,5 +60,77 @@ contract math{
         return a**b;
     }
 
+
+    //位操作
+
+    uint8 a = 3;
+    uint8 b =4;
+
+    /**
+        位与
+        两个数字之间每一位进行比较 1 1 => 1 同位都是1才返回1
+     */
+    function weiyu()
+        public
+        pure
+        returns(uint)
+    {
+        return 3 & 4;
+    }
+    /**
+        位或
+        两个数字之间每一位进行比较 1 0 => 1 只要有一位 是1才返回1
+     */
+    function weihuo()
+        public
+        pure
+        returns(uint)
+    {
+        return 3 | 4;
+    }
+    /**
+        位反
+        位存储的数据0 1互换
+     */
+    function weifan()
+        public
+        view
+        returns(uint)
+    {
+        return ~a;
+    }
+    /**
+        位亦或
+        两位相等位0 不等为1
+     */
+    function weiyihuo()
+        public
+        pure
+        returns(uint)
+    {
+        return 3^4;
+    }
+    /**
+        位左移
+        位整体左移动n位
+     */
+    function weiZuoYi()
+        public
+        pure
+        returns(uint)
+    {
+        return 3<<1;
+    }
+    /**
+        位右移
+        位整体右移动n位
+     */
+    function weiYuoYi()
+        public
+        pure
+        returns(uint)
+    {
+        return 3>>1 ;
+    }
 
 }
